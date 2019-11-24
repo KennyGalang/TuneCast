1bf888c96dea2e9bcf1f6f177e497054c523c1f5
Basil Arif <bbarif@edu.uwaterloo.ca>
2019-11-24 04:54:26 -0500

    Revert "Kenny's Stuff"
    This reverts commit f0bf8a46cf3ed8b5c1567377e4f38d618db390dc.

--- TuneCast/AppDelegate.swift
+++ TuneCast/AppDelegate.swift
@@ -12,163 +12,85 @@ import AsyncDisplayKit
 import Firebase
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate, SPTAppRemoteDelegate {
-    private let redirectUri = URL(string:"comspotifytestsdk://")!
-        //    private let clientIdentifier = "089d841ccc194c10a77afad9e1c11d54"
-        private let clientIdentifier = "99eec6e883ad49fb90367400f1b638ef"
-        private let name = "Now Playing View"
-            
-        // keys
-        static private let kAccessTokenKey = "access-token-key"
-        
-        var window: UIWindow?
-        var window2: UIWindow?
-        
-        func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any]) -> Bool {
-            let parameters = appRemote.authorizationParameters(from: url);
+class AppDelegate: UIResponder, UIApplicationDelegate {
 
-            if let access_token = parameters?[SPTAppRemoteAccessTokenKey] {
-                appRemote.connectionParameters.accessToken = access_token
-                AppDelegate.self.accessToken = access_token
-            } else if let error_description = parameters?[SPTAppRemoteErrorDescriptionKey] {
-                playerViewController.showError(error_description);
-            }
-            
-            return true
-        }
-        
-        func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-            FirebaseApp.configure()
-            return true
-           }
-        
-        
-        func applicationWillResignActive(_ application: UIApplication) {
-            // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-            // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
-        }
+    var window: UIWindow?
 
-        func applicationDidEnterBackground(_ application: UIApplication) {
-            // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-            // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-        }
 
-        func applicationWillEnterForeground(_ application: UIApplication) {
-            // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-        }
-
-        func applicationDidBecomeActive(_ application: UIApplication) {
-            // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-        }
-
-        func applicationWillTerminate(_ application: UIApplication) {
-            // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-            // Saves changes in the application's managed object context before the application terminates.
-            self.saveContext()
-        }
-
-        // MARK: - Core Data stack
-
-        lazy var persistentContainer: NSPersistentContainer = {
-            /*
-             The persistent container for the application. This implementation
-             creates and returns a container, having loaded the store for the
-             application to it. This property is optional since there are legitimate
-             error conditions that could cause the creation of the store to fail.
-            */
-            let container = NSPersistentContainer(name: "TuneCast")
-            container.loadPersistentStores(completionHandler: { (storeDescription, error) in
-                if let error = error as NSError? {
-                    // Replace this implementation with code to handle the error appropriately.
-                    // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                     
-                    /*
-                     Typical reasons for an error here include:
-                     * The parent directory does not exist, cannot be created, or disallows writing.
-                     * The persistent store is not accessible, due to permissions or data protection when the device is locked.
-                     * The device is out of space.
-                     * The store could not be migrated to the current model version.
-                     Check the error message to determine what the actual problem was.
-                     */
-                    fatalError("Unresolved error \(error), \(error.userInfo)")
-                }
-            })
-            return container
-        }()
-        
-        // MARK: - Core Data Saving support
-
-        func saveContext () {
-            let context = persistentContainer.viewContext
-            if context.hasChanges {
-                do {
-                    try context.save()
-                } catch {
-                    // Replace this implementation with code to handle the error appropriately.
-                    // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                    let nserror = error as NSError
-                    fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
-                }
-            }
-        }
-        
-
-        static var accessToken = UserDefaults.standard.string(forKey: kAccessTokenKey) {
-            didSet {
-                let defaults = UserDefaults.standard
-                defaults.set(accessToken, forKey: AppDelegate.kAccessTokenKey)
-                defaults.synchronize()
-            }
-        }
-            
-            
-            var playerViewController: SongPlayerViewController {
-                get {
-                    let navController = self.window2?.rootViewController?.children[0] as! UINavigationController
-                    return navController.topViewController as! SongPlayerViewController
-                }
-            }
-            
-    //        var window: UIWindow?
-            
-            lazy var appRemote: SPTAppRemote = {
-                let configuration = SPTConfiguration(clientID: self.clientIdentifier, redirectURL: self.redirectUri)
-                let appRemote = SPTAppRemote(configuration: configuration, logLevel: .debug)
-                appRemote.connectionParameters.accessToken = AppDelegate.self.accessToken
-                appRemote.delegate = self
-                return appRemote
-            }()
-            
-            class var sharedInstance: AppDelegate {
-                get {
-                    return UIApplication.shared.delegate as! AppDelegate
-                }
-            }
-            
-            
-
-            func connect() {
-                playerViewController.appRemoteConnecting()
-                appRemote.connect()
-            }
-
-            // MARK: AppRemoteDelegate
-            
-            func appRemoteDidEstablishConnection(_ appRemote: SPTAppRemote) {
-                self.appRemote = appRemote
-                playerViewController.appRemoteConnected()
-            }
-            
-            func appRemote(_ appRemote: SPTAppRemote, didFailConnectionAttemptWithError error: Error?) {
-                print("didFailConnectionAttemptWithError")
-                playerViewController.appRemoteDisconnect()
-            }
-            
-            func appRemote(_ appRemote: SPTAppRemote, didDisconnectWithError error: Error?) {
-                print("didDisconnectWithError")
-                playerViewController.appRemoteDisconnect()
-            }
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        FirebaseApp.configure()
+        return true
+       }
     
+
+    func applicationWillResignActive(_ application: UIApplication) {
+        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+    }
+
+    func applicationDidEnterBackground(_ application: UIApplication) {
+        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    }
+
+    func applicationWillEnterForeground(_ application: UIApplication) {
+        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+    }
+
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    }
+
+    func applicationWillTerminate(_ application: UIApplication) {
+        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+        // Saves changes in the application's managed object context before the application terminates.
+        self.saveContext()
+    }
+
+    // MARK: - Core Data stack
+
+    lazy var persistentContainer: NSPersistentContainer = {
+        /*
+         The persistent container for the application. This implementation
+         creates and returns a container, having loaded the store for the
+         application to it. This property is optional since there are legitimate
+         error conditions that could cause the creation of the store to fail.
+        */
+        let container = NSPersistentContainer(name: "TuneCast")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                // Replace this implementation with code to handle the error appropriately.
+                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                 
+                /*
+                 Typical reasons for an error here include:
+                 * The parent directory does not exist, cannot be created, or disallows writing.
+                 * The persistent store is not accessible, due to permissions or data protection when the device is locked.
+                 * The device is out of space.
+                 * The store could not be migrated to the current model version.
+                 Check the error message to determine what the actual problem was.
+                 */
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
+
+    // MARK: - Core Data Saving support
+
+    func saveContext () {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                // Replace this implementation with code to handle the error appropriately.
+                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
 
 }
 
